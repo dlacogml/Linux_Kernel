4f15de1afc1fd1d49a92edecd3517fa0d45c8250
Ben <ben@Bens-MacBook-Pro.local>
2020-03-22 14:33:39 -0400

    pic

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -9,10 +9,7 @@ uint8_t master_mask; /* IRQs 0-7  */
 uint8_t slave_mask;  /* IRQs 8-15 */
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
-    // unsigned char a1, a2;
- 
-	// a1 = inb(MASTER_8259_DATA);                        // save masks
-	// a2 = inb(SLAVE_8259_DATA);
+    
     master_mask = 0xff;
     slave_mask = 0xff;
 
@@ -29,42 +26,45 @@ void i8259_init(void) {
     outb(ICW3_SLAVE, SLAVE_8259_DATA);
     outb(ICW4, SLAVE_8259_DATA);
 
-    // udelay(100);
-    // outb(cached_21, MASTER_8259_PORT + 1);
+    printf("master: %x\n", master_mask);
+    printf("slave: %x\n", slave_mask);
 
 
 }
 
 /* Enable (unmask) the specified IRQ */
-void enable_irq(uint32_t irq_num) {
+void disable_irq(uint32_t irq_num) {
  
     if(irq_num < 8) {
         master_mask = master_mask | (1 << irq_num);
-        outb(MASTER_8259_DATA, master_mask);
+        outb(master_mask, MASTER_8259_DATA);
     } else {
         irq_num -= 8;
         slave_mask = slave_mask | (1 << irq_num);
-        outb(SLAVE_8259_DATA, slave_mask);
+        outb(slave_mask, SLAVE_8259_DATA);
     }
-
+    printf("master: %x\n", master_mask);
+    printf("slave: %x\n", slave_mask);
 }
 
 /* Disable (mask) the specified IRQ */
-void disable_irq(uint32_t irq_num) {
+void enable_irq(uint32_t irq_num) {
     if(irq_num < 8) {
         master_mask = master_mask & ~(1 << irq_num);
-        outb(MASTER_8259_DATA, master_mask);
+        outb(master_mask, MASTER_8259_DATA);
     } else {
         irq_num -= 8;
         slave_mask = slave_mask & ~(1 << irq_num);
-        outb(SLAVE_8259_DATA, slave_mask);
+        outb(slave_mask, SLAVE_8259_DATA);
     }
+    printf("master: %x\n", master_mask);
+    printf("slave: %x\n", slave_mask);
 }
 
 /* Send end-of-interrupt signal for the specified IRQ */
 void send_eoi(uint32_t irq_num) {
     if(irq_num >= 8){
-		outb(SLAVE_8259_PORT, EOI);
+		outb(EOI, SLAVE_8259_PORT);
     }
-	outb(MASTER_8259_PORT, EOI);
+	outb(EOI, MASTER_8259_PORT);
 }
