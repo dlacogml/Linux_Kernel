205a349ff79a12df1380133ae44bd004a0e703f0
Ben <ben@Bens-MacBook-Pro.local>
2020-03-21 20:58:09 -0400

    idt

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -15,9 +15,29 @@
 /* Check if the bit BIT in FLAGS is set. */
 #define CHECK_FLAG(flags, bit)   ((flags) & (1 << (bit)))
 
-int divide_by_zero_handler();
-
-
+void handler0();
+void handler1();
+void handler2();
+void handler3();
+void handler4();
+void handler5();
+void handler6();
+void handler7();
+void handler8();
+void handler9();
+void handler10();
+void handler11();
+void handler12();
+void handler13();
+void handler14();
+void handler15();
+void handler16();
+void handler17();
+void handler18();
+void handler19();
+void handler32();
+void handler33();
+void handler128();
 /* Check if MAGIC is valid and print the Multiboot information structure
    pointed by ADDR. */
 void entry(unsigned long magic, unsigned long addr) {
@@ -141,6 +161,8 @@ void entry(unsigned long magic, unsigned long addr) {
 
     {
         idt_desc_t desc;
+
+        /* Exceptions */
         desc.present = 0x1;
         desc.dpl = 0x0;
         desc.reserved0 = 0x0;
@@ -151,9 +173,79 @@ void entry(unsigned long magic, unsigned long addr) {
         desc.reserved4 = 0x0;
         desc.seg_selector = KERNEL_CS;
 
-        SET_IDT_ENTRY(desc, divide_by_zero_handler);
+        SET_IDT_ENTRY(desc, handler0);
         idt[0] = desc;
 
+        SET_IDT_ENTRY(desc, handler1);
+        idt[1] = desc;
+
+        SET_IDT_ENTRY(desc, handler3);
+        idt[3] = desc;
+
+        SET_IDT_ENTRY(desc, handler4);
+        idt[4] = desc;
+
+        SET_IDT_ENTRY(desc, handler5);
+        idt[5] = desc;
+
+        SET_IDT_ENTRY(desc, handler6);
+        idt[6] = desc;
+
+        SET_IDT_ENTRY(desc, handler7);
+        idt[7] = desc;
+
+        SET_IDT_ENTRY(desc, handler8);
+        idt[8] = desc;
+
+        SET_IDT_ENTRY(desc, handler10);
+        idt[10] = desc;
+
+        SET_IDT_ENTRY(desc, handler11);
+        idt[11] = desc;
+
+        SET_IDT_ENTRY(desc, handler12);
+        idt[12] = desc;
+
+        SET_IDT_ENTRY(desc, handler13);
+        idt[13] = desc;
+
+        SET_IDT_ENTRY(desc, handler14);
+        idt[14] = desc;
+
+        SET_IDT_ENTRY(desc, handler16);
+        idt[16] = desc;
+
+        SET_IDT_ENTRY(desc, handler17);
+        idt[17] = desc;
+
+        SET_IDT_ENTRY(desc, handler18);
+        idt[18] = desc;
+
+        SET_IDT_ENTRY(desc, handler19);
+        idt[19] = desc;
+        
+
+        /* Interrupts */
+        desc.reserved3 = 0x0; // change to interrupt
+        SET_IDT_ENTRY(desc, handler2);
+        idt[2] = desc;
+
+        SET_IDT_ENTRY(desc, handler32);
+        idt[32] = desc;
+
+        SET_IDT_ENTRY(desc, handler33);
+        idt[33] = desc;
+
+
+        /* System Call */
+        desc.dpl = 0x3;
+        desc.size = 0x0;
+        desc.reserved2 = 0x0;
+        desc.reserved3 = 0x1;
+        desc.seg_selector = KERNEL_TSS;
+
+        SET_IDT_ENTRY(desc, handler128);
+        idt[0x80] = desc;
     }
 
     /* Init the PIC */
@@ -166,8 +258,8 @@ void entry(unsigned long magic, unsigned long addr) {
     /* Do not enable the following until after you have set up your
      * IDT correctly otherwise QEMU will triple fault and simple close
      * without showing you any output */
-    /*printf("Enabling Interrupts\n");
-    sti();*/
+    // printf("Enabling Interrupts\n");
+    // sti();
 
 #ifdef RUN_TESTS
     /* Run tests */
@@ -180,6 +272,151 @@ void entry(unsigned long magic, unsigned long addr) {
 }
 
 
-int divide_by_zero_handler(){
-    return 0;
+void handler0(){
+    printf("Divide by zero");
+    while(1){
+
+    }
+}
+
+void handler1(){
+    printf("Debug Exception");
+    while(1){
+
+    }
+}
+
+void handler2(){
+    printf("NMI Interrupt");
+    while(1){
+
+    }
+}
+void handler3(){
+    printf("Breakpoint Exception");
+    while(1){
+
+    }
+}
+void handler4(){
+    printf("Overflow Exception");
+    while(1){
+
+    }
+}
+
+void handler5(){
+    printf("BOUND Range Exceeded Exception");
+    while(1){
+
+    }
+}
+void handler6(){
+    printf("Invalid Opcode Exception");
+    while(1){
+
+    }
+}
+void handler7(){
+    printf("Device Not Available Exception");
+    while(1){
+
+    }
+}
+void handler8(){
+    printf("Double Fault Exception");
+    while(1){
+
+    }
+}
+void handler9(){
+    printf("Debug Exception");
+    while(1){
+
+    }
+}
+void handler10(){
+    printf("Invalid TSS Exception");
+    while(1){
+
+    }
+}
+
+void handler11(){
+    printf("Segment Not Present");
+    while(1){
+
+    }
+}
+void handler12(){
+    printf("Stack Fault Exception");
+    while(1){
+
+    }
+}
+void handler13(){
+    printf("General Protection Exception");
+    while(1){
+
+    }
+}
+
+void handler14(){
+    printf("Page Fault Exception");
+    while(1){
+
+    }
+}
+
+void handler15(){
+    printf("Nothing");
+    while(1){
+
+    }
+}
+void handler16(){
+    printf("x87 FPU Floating Point Error");
+    while(1){
+
+    }
+}
+void handler17(){
+    printf("Alignment Check Exception");
+    while(1){
+
+    }
+}
+
+void handler18(){
+    printf("Machine Check Exception");
+    while(1){
+
+    }
+}
+void handler19(){
+    printf("SIMD Floating Point Exception");
+    while(1){
+
+    }
+}
+
+void handler32(){
+    printf("Keyboard");
+    while(1){
+
+    }
+}
+
+void handler33(){
+    printf("RTC");
+    while(1){
+
+    }
+}
+
+void handler128(){
+    printf("System Call");
+    while(1){
+
+    }
 }
