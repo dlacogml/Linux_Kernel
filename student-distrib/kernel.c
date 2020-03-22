2057ace8714a801abba8aa7c383a0e1c8d7b0d5c
Ben <ben@Bens-MacBook-Pro.local>
2020-03-22 16:31:16 -0400

    keyboard

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -14,7 +14,45 @@
 /* Macros. */
 /* Check if the bit BIT in FLAGS is set. */
 #define CHECK_FLAG(flags, bit)   ((flags) & (1 << (bit)))
-
+unsigned char keyboard_map[128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',     /* 9 */
+  '9', '0', '-', '=', '\b',     /* Backspace */
+  '\t',                 /* Tab */
+  'q', 'w', 'e', 'r',   /* 19 */
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', /* Enter key */
+    0,                  /* 29   - Control */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',     /* 39 */
+ '\'', '`',   0,                /* Left shift */
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n',                    /* 49 */
+  'm', ',', '.', '/',   0,                              /* Right shift */
+  '*',
+    0,  /* Alt */
+  ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+  '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+  '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
 void handler0();
 void handler1();
 void handler2();
@@ -176,12 +214,21 @@ void handler32(){
     }
 }
 void handler33(){
-    disable_irq(1);
+    // disable_irq(1);
+    // send_eoi(1);
+    // sti();
+    // printf("Keyboard");
+    // enable_irq(1);
+    // send_eoi(1);
+    cli();
+    char key = inb(0x60);
+    if (key > 0 && keyboard_map[key]){
+        printf("%c", keyboard_map[key]);
+    }
+    // printf("%d\n", key);
     send_eoi(1);
     sti();
-    printf("Keyboard");
-    enable_irq(1);
-    send_eoi(1);
+    // enable_irq(1);
 }
 
 void handler40(){
