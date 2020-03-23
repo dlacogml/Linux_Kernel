07ba35ba830bd65f6e4dfd075820a533e6564dce
Ben <ben@Bens-MBP.fios-router.home>
2020-03-22 23:42:18 -0400

    paging

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -11,29 +11,7 @@
 #include "keyboard.h"
 
 
-void handler0();
-void handler1();
-void handler2();
-void handler3();
-void handler4();
-void handler5();
-void handler6();
-void handler7();
-void handler8();
-void handler9();
-void handler10();
-void handler11();
-void handler12();
-void handler13();
-void handler14();
-void handler15();
-void handler16();
-void handler17();
-void handler18();
-void handler19();
-void handler33();
-void handler40();
-void handler128();
+
 
 void handler0(){
     printf("Divide by zero");
--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -8,6 +8,28 @@
 #define KERNEL_CS   0x0010
 
 void init_idt();
-
+void handler0();
+void handler1();
+void handler2();
+void handler3();
+void handler4();
+void handler5();
+void handler6();
+void handler7();
+void handler8();
+void handler9();
+void handler10();
+void handler11();
+void handler12();
+void handler13();
+void handler14();
+void handler15();
+void handler16();
+void handler17();
+void handler18();
+void handler19();
+void handler33();
+void handler40();
+void handler128();
 
 #endif
