005f4c4399c41868cb27c69e6bedf579977bf46a
Ben <ben@Bens-MBP.fios-router.home>
2020-03-23 16:20:20 -0400

    added linkage

---
+++ student-distrib/idt_asm.h
@@ -0,0 +1,14 @@
+#include "keyboard.h"
+#include "rtc.h"
+#include "idt.h"
+#include "lib.h"
+
+#ifndef IDT_ASM_H
+#define IDT_ASM_H
+
+void asmHandler2();
+void asmHandler33();
+void asmHandler40();
+void asmHandler128();
+
+#endif 
