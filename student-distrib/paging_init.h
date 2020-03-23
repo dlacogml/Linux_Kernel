537f01976f6a656a85bb537d29e6e98132ff7863
Kevin Choi <kevinsc2@illinois.edu>
2020-03-23 16:29:31 -0500

    paging_init.h commented

--- student-distrib/paging_init.h
+++ student-distrib/paging_init.h
@@ -3,7 +3,10 @@
 #define PAGING_INIT_H
 
 
-
+/* 
+ * these two functions sets the correct bits in the control
+ * registers to enable paging
+ */
 void loadPageDirectory(unsigned int*);
 void enablePaging();
 
