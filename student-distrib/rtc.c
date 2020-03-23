ca0a6a0f0913a943ea0a8873406dd87a13e586ad
chaehee2 <chaehee2@illinois.edu>
2020-03-23 18:38:54 -0400

    comment rtc

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -8,9 +8,10 @@
 #include "rtc.h"
 #include "i8259.h"
 
+#define RTC_IRQ     8
 
 /* rtc_init
- * DESCRIPTION: initializes the RTC
+ * DESCRIPTION: initializes the RTC, enables interrupt request for rtc
  * INPUT: nothing
  * OUTPUT: nothing
  * SIDE EFFECT: 
@@ -18,7 +19,7 @@
 void rtc_init() {
     
     cli();	
-    enable_irq(8);
+    enable_irq(RTC_IRQ);
     outb(RTC_B, RTC_PORT);		
     char prev = inb(RTC_DATA);	
     outb(RTC_B, RTC_PORT);		
@@ -27,6 +28,12 @@ void rtc_init() {
     
 }
 
+/* rtc_set_rate
+ * DESCRIPTION: sets the rate of the rtc
+ * INPUT: rate
+ * OUTPUT: nothing
+ * SIDE EFFECT: 
+ */
 void rtc_set_rate(unsigned char rate) {
 
     rate &= 0x0F; //rate is and-ed with 0x0F (15 base 10) to make sure rate is not above 15
@@ -40,13 +47,19 @@ void rtc_set_rate(unsigned char rate) {
 
 }
 
+/* handler40
+ * DESCRIPTION: handler for RTC interrupts
+ * INPUT: nothing
+ * OUTPUT: nothing
+ * SIDE EFFECT: 
+ */
 void handler40(){
-    cli();
-    printf("RTC");
-    test_interrupts();
-    outb(RTC_C, RTC_PORT);	
-    inb(RTC_DATA);		
-    send_eoi(8);
+    cli();            
+
+    test_interrupts();      // checks if the rtc works
+    outb(RTC_C, RTC_PORT);	        // set index to register A, disable NMI
+    inb(RTC_DATA);		// retrieve rtc_data
+    send_eoi(8);           // end of interrupt
+    
     sti();
 }
-
