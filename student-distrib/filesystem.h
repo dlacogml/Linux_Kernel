cca04c27828d4a723da09df7d681e40dcfaea178
Ben <ben@Bens-MBP.fios-router.home>
2020-03-25 19:24:02 -0400

    filesystem

---
+++ student-distrib/filesystem.h
@@ -0,0 +1,43 @@
+/* 
+ * 
+ * idt.h - Defines used in interactions with the idt interrupt
+ * 
+ */
+
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
+#include "lib.h"
+
+#define FILENAME_LENGTH     32
+
+typedef struct dentry {
+    int8_t filename[FILENAME_LENGTH];
+    int32_t filetype;
+    int32_t inode_num;
+    int8_t reserved[24];
+} dentry_t;
+
+typedef struct inode {
+    int32_t length;
+    int32_t data_block_num[1023];
+} inode_t;
+
+
+
+int32_t file_open();
+int32_t file_close();
+int32_t file_read();
+int32_t file_write();
+
+int32_t dir_open();
+int32_t dir_close();
+int32_t dir_read();
+int32_t dir_write();
+
+int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry);
+int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
+int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+
+
+
+#endif
